ae8f8a18c020fc2d48648993e7b000af49529794
Jeethi Nair <jeethi@umich.edu>
2018-10-26 11:18:03 -0400

    Added program to commit

---
+++ rpn.py
@@ -0,0 +1,25 @@
+def calculate(arg):
+    # stack for calculator
+    stack = []
+
+    # tokenize input
+    tokens = arg.split()
+
+    # process tokens
+    for token in tokens:
+        try:
+            value = int(token)
+            stack.append(value)
+        except ValueError:
+            val1 = stack.pop()
+            val2 stack.pop()
+            result = val1 + val2
+            stack.append(result)
+            return stack[0]
+
+def main():
+    while True:
+        calculate(input('rpn calc>'))
+
+if __name__ == '__main__':
+    main()
