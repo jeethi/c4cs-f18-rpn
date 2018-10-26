d180b7b11400b22309f9659717a5ae4a4ac4dac8
Jeethi Nair <jeethi@umich.edu>
2018-10-26 11:53:15 -0400

    multiply and divide cleanup

--- test_rpn.py
+++ test_rpn.py
@@ -14,5 +14,8 @@ class TestBasics(unittest.TestCase):
     def test_div(self):
         result = rpn.calculate('4 2 /')
         self.assertEqual(2, result)
+    def test_chain(self):
+        result = rpn.calculate('1 1 + 2 *')
+        self.assertEqual(4, result)
 
 # TODO: Add test for subtract
